bc642993f92c46aafbad14e92506acffba41aa39
jv-aquino <jvaquinocontact@gmail.com>
2023-05-16 08:30:00 -0300

    + improve ui and auth system

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -5,10 +5,9 @@ import Footer from "@/components/Footer"
 import { useRouter } from 'next/navigation'
 
 import supabase from "@/lib/supabase"
-import { useState } from "react";
+import { useEffect } from "react";
 
 function Login() {
-  const [user, setUser] = useState();
   const router = useRouter();
 
   const handleLogin = async e => {
@@ -22,40 +21,42 @@ function Login() {
     if (error) {
       console.log(error);
     } else {
-      console.log(data);
-      setUser(data);
       router.push('/')
     }
   }
 
-  if (user) {
-    router.push('/')
-    return null
-  }
-
   return (
-    <div>
+    <>
       <h1 className="">Login Administrativo</h1>
       <form onSubmit={handleLogin} className="p-4 flex flex-col gap-3 
-      rounded bg-dark-blue-500 text-black">
+      rounded-md bg-dark-blue-500 shadow-lg text-black text-lg">
         <label>
-          Email: <input type="email" id="email" />
+          Email: <input type="email" id="email" placeholder="exemplo@unesp.br" />
         </label>
         <label>
           Senha: <input type="password" id="senha" />
         </label>
-        <button type="submit" className="px-3 py-1 w-fit m-auto rounded   bg-light-blue-500">Login</button>
+        <button type="submit" className="px-3 py-1 w-fit m-auto rounded   bg-light-blue-500 font-bold font-header text-2xl">Login</button>
       </form>
-    </div>
+    </>
   )
 }
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const { data, error } = supabase.auth.getSession();
+    if (data) {
+      console.log("ebaaa");
+    }
+  }, [router]);
+
   return (
     <>
       <Navbar />
       
-      <main className="flex justify-center">
+      <main className="flex flex-col items-center">
         <Login />
       </main>
 
